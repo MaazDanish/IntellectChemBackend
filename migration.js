1b96b217f95a51e372287e0eb01be492ab854d98
mohd@webanix.in <mohd@webanix.in>
2025-07-13 22:18:11 +0530

    added directory types of esmodule

--- migration.js
+++ migration.js
@@ -1,16 +1,22 @@
 import mysql from 'mysql2';
 import migration from 'mysql-migrations';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
+// Simulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-var connection = mysql.createPool({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
-migration.init(connection, __dirname + "/src/migrations", function () {
-  console.log("finished running migrations");
+migration.init(connection, path.join(__dirname, "src", "migrations"), function () {
+  console.log("✅ Finished running migrations");
 });
